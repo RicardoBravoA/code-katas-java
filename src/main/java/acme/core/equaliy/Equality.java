7b037a69e48b242c542672936963f6623aad2dfd
Oliver Schrenk <oliver.schrenk@gmail.com>
2013-02-22 11:31:49 +0100

    More equality checks

--- src/main/java/acme/core/equaliy/Equality.java
+++ src/main/java/acme/core/equaliy/Equality.java
@@ -6,7 +6,11 @@ public class Equality {
 		System.out.println(new Integer(1) == new Integer(1)); // false
 		System.out.println(new Integer(1).equals(new Integer(1))); // true
 		System.out.println("FOO" == "FOO"); // true
+		System.out.println("FOO" == "FOO".intern()); // true
+		System.out.println("FOO".intern() == "FOO".intern()); // true
 		System.out.println("FOO" == new String("FOO")); // false
+		System.out.println("FOO" == new String("FOO").intern()); // true
+		System.out.println(new String("FOO") == new String("FOO")); // false
 		System.out.println("FOO".equals("FOO")); // true
 		System.out.println("FOO".equals(new String(("FOO")))); // true
 		System.out.println("FOO".hashCode() == new String("FOO").hashCode()); // true
