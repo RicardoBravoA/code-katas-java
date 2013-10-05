4e6a71f52ac0624a9fa303a11d24e5bd2b59b4c4
Oliver Schrenk <oliver.schrenk@gmail.com>
2013-10-05 15:37:55 +0200

    Another go at reversing a string with a char array

---
+++ src/main/java/acme/strings/reverse/AnotherArraySwappingReverser.java
@@ -0,0 +1,22 @@
+package acme.strings.reverse;
+
+import acme.api.Reverser;
+
+public class AnotherArraySwappingReverser implements Reverser<String> {
+
+	@Override
+	public String reverse(final String str) {
+		if ((null == str) || (str.length() <= 1)) {
+			return str;
+		}
+		final char[] chars = str.toCharArray();
+		int right = chars.length - 1;
+		for (int left = 0; left < right; left++) {
+			final char swap = chars[left];
+			chars[left] = chars[right];
+			chars[right--] = swap;
+		}
+		return new String(chars);
+	}
+
+}
--- src/main/java/acme/strings/reverse/ArraySwappingReverser.java
+++ src/main/java/acme/strings/reverse/ArraySwappingReverser.java
@@ -4,18 +4,25 @@ import acme.api.Reverser;
 
 public class ArraySwappingReverser implements Reverser<String> {
 
-	public String reverse(final String str) {
-		if ((null == str) || (str.length() <= 1)) {
-			return str;
+	@Override
+	public String reverse(String s) {
+
+		if (s == null || s.length() == 1) {
+			return s;
 		}
-		final char[] chars = str.toCharArray();
-		int right = chars.length - 1;
-		for (int left = 0; left < right; left++) {
-			final char swap = chars[left];
-			chars[left] = chars[right];
-			chars[right--] = swap;
+
+		char[] c = s.toCharArray();
+		int l = c.length;
+		char swap;
+
+		for (int i = 0; i < l / 2; i++) {
+			swap = c[i];
+			c[i] = c[l - i];
+			c[l - i] = swap;
 		}
-		return new String(chars);
+
+		return new String(c);
+
 	}
 
 }
