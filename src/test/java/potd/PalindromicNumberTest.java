92f293b86066c833601c57f36c3a60e37ac5dcfe
Oliver Schrenk <oliver.schrenk@gmail.com>
2014-03-26 18:31:39 +0100

    Remove nonsensical test

--- src/test/java/potd/PalindromicNumberTest.java
+++ src/test/java/potd/PalindromicNumberTest.java
@@ -3,18 +3,12 @@ package potd;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class PalindromicNumberTest {
 
 	PalindromicNumber palindromNumber = new PalindromicNumber();
-
-	@Test
-	public void testIsPalindrom() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void isPalindromic() {
